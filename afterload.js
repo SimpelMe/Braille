28e14837ea227859b77589fe4c95f19c7f653f1b
Conrad Zelck <git@simpel.cc>
2019-09-18 11:16:46 +0200

    beautify all files
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- afterload.js
+++ afterload.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    window.addEventListener("resize", scaleBody);
-    scaleBody(); // once at start
-	if (isMobileDevice()) deleteTableKeyboard(); // wenn mobil dannn Tabelle obendrüber ausblenden
-	if (isMobileDevice()) document.getElementById ("Manual").style.fontSize = "1.3vw";  
+  window.addEventListener("resize", scaleBody);
+  scaleBody(); // once at start
+  if (isMobileDevice()) deleteTableKeyboard(); // wenn mobil dannn Tabelle obendrüber ausblenden
+  if (isMobileDevice()) document.getElementById("Manual").style.fontSize = "1.3vw";
 }
