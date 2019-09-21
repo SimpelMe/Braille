8f595890882b3948a421005a23f20f45e842398f
Conrad Zelck <git@simpel.cc>
2019-09-21 23:06:36 +0200

    add brailleline.js to dynamically create the cells of the brailleline
    Signed-off-by: Conrad Zelck <git@simpel.cc>

---
+++ brailleline.js
@@ -0,0 +1,16 @@
+document.write("<tr>");
+for (var i = 1; i < limitZellen + 1; i++) {
+  document.write('<td class="zelle coursorpointer">');
+  document.write('<img src="hilfspunkt.svg" name="p' + i +'_1" alt="·" title="1" onClick="klick(' + i +',1);">');
+  document.write('<img src="hilfspunkt.svg" name="p' + i +'_4" alt="·" title="4" onClick="klick(' + i +',4);"><br>');
+  document.write('<img src="hilfspunkt.svg" name="p' + i +'_2" alt="·" title="1" onClick="klick(' + i +',2);">');
+  document.write('<img src="hilfspunkt.svg" name="p' + i +'_5" alt="·" title="4" onClick="klick(' + i +',5);"><br>');
+  document.write('<img src="hilfspunkt.svg" name="p' + i +'_3" alt="·" title="1" onClick="klick(' + i +',3);">');
+  document.write('<img src="hilfspunkt.svg" name="p' + i +'_6" alt="·" title="4" onClick="klick(' + i +',6);"></td>');
+}
+document.write('</tr>');
+document.write("<tr>");
+for (var i = 1; i < limitZellen + 1; i++) {
+  document.write('<td class="zelle"><input name="z' + i +'" onclick="aend(' + i +')" value=" " maxlength="4" class="output bgwhite coursorpointer" onfocus="if(this.blur)this.blur()" type="reset"></td>');
+}
+document.write("</tr>");
