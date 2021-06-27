9e640f20cec120dec8626c330e91abcde7b07c32
Conrad Zelck <git@simpel.cc>
2021-06-27 13:57:25 +0200

    refact brailleline.js: put content into function
    This functionallity is needed to rebuild the braille line after resizing
    it. So all code should be into an own function.
    
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- brailleline.js
+++ brailleline.js
@@ -1,16 +1,88 @@
-document.write("<tr>");
-for (var i = 1; i < limitZellen + 1; i++) {
-  document.write('<td class="zelle braille coursorpointer">');
-  document.write('<img class="smallpoint" src="hilfspunkt.svg" name="p' + i +'_1" alt="·" title="1" onClick="klick(' + i +',1);">');
-  document.write('<img class="smallpoint" src="hilfspunkt.svg" name="p' + i +'_4" alt="·" title="4" onClick="klick(' + i +',4);"><br>');
-  document.write('<img class="smallpoint" src="hilfspunkt.svg" name="p' + i +'_2" alt="·" title="1" onClick="klick(' + i +',2);">');
-  document.write('<img class="smallpoint" src="hilfspunkt.svg" name="p' + i +'_5" alt="·" title="4" onClick="klick(' + i +',5);"><br>');
-  document.write('<img class="smallpoint" src="hilfspunkt.svg" name="p' + i +'_3" alt="·" title="1" onClick="klick(' + i +',3);">');
-  document.write('<img class="smallpoint" src="hilfspunkt.svg" name="p' + i +'_6" alt="·" title="4" onClick="klick(' + i +',6);"></td>');
+function writeBrailleline() {
+  var brailleTable = document.getElementById("brailleline");
+  var row = brailleTable.insertRow();
+
+  for (var i = 1; i < limitZellen + 1; i++) {
+    var cell = row.insertCell();
+    cell.classList.add("zelle");
+    cell.classList.add("braille");
+    cell.classList.add("coursorpointer");
+
+    var img = document.createElement('img');
+    img.setAttribute("class", "smallpoint");
+    img.setAttribute("src", "hilfspunkt.svg");
+    img.setAttribute("alt", "·");
+    img.setAttribute("title", "1");
+    img.setAttribute("name", "p" + i + "_1");
+    img.setAttribute("onclick", "klick(" + i + ", 1)");
+    cell.appendChild(img);
+
+    var img = document.createElement('img');
+    img.setAttribute("class", "smallpoint");
+    img.setAttribute("src", "hilfspunkt.svg");
+    img.setAttribute("alt", "·");
+    img.setAttribute("title", "4");
+    img.setAttribute("name", "p" + i + "_4");
+    img.setAttribute("onclick", "klick(" + i + ", 4)");
+    cell.appendChild(img);
+
+    cell.innerHTML += "<br>";
+
+    var img = document.createElement('img');
+    img.setAttribute("class", "smallpoint");
+    img.setAttribute("src", "hilfspunkt.svg");
+    img.setAttribute("alt", "·");
+    img.setAttribute("title", "2");
+    img.setAttribute("name", "p" + i + "_2");
+    img.setAttribute("onclick", "klick(" + i + ", 2)");
+    cell.appendChild(img);
+
+    var img = document.createElement('img');
+    img.setAttribute("class", "smallpoint");
+    img.setAttribute("src", "hilfspunkt.svg");
+    img.setAttribute("alt", "·");
+    img.setAttribute("title", "5");
+    img.setAttribute("name", "p" + i + "_5");
+    img.setAttribute("onclick", "klick(" + i + ", 5)");
+    cell.appendChild(img);
+
+    cell.innerHTML += "<br>";
+
+    var img = document.createElement('img');
+    img.setAttribute("class", "smallpoint");
+    img.setAttribute("src", "hilfspunkt.svg");
+    img.setAttribute("alt", "·");
+    img.setAttribute("title", "3");
+    img.setAttribute("name", "p" + i + "_3");
+    img.setAttribute("onclick", "klick(" + i + ", 3)");
+    cell.appendChild(img);
+
+    var img = document.createElement('img');
+    img.setAttribute("class", "smallpoint");
+    img.setAttribute("src", "hilfspunkt.svg");
+    img.setAttribute("alt", "·");
+    img.setAttribute("title", "6");
+    img.setAttribute("name", "p" + i + "_6");
+    img.setAttribute("onclick", "klick(" + i + ", 6)");
+    cell.appendChild(img);
+  }
+
+  var row = brailleTable.insertRow();
+  for (var i = 1; i < limitZellen + 1; i++) {
+    var cell = row.insertCell();
+    cell.classList.add("zelle");
+    var input = document.createElement('input');
+    input.classList.add("output");
+    input.classList.add("bgtransparent");
+    input.classList.add("coursorpointer");
+    input.setAttribute("name", "z" + i)
+    input.setAttribute("value", " ")
+    input.setAttribute("maxlength", "4")
+    input.setAttribute("type", "reset")
+    input.setAttribute("onclick", "aend(" + i + ")");
+    input.setAttribute("onfocus", "if(this.blur)this.blur()");
+    cell.appendChild(input);
+  }
 }
-document.write('</tr>');
-document.write("<tr>");
-for (var i = 1; i < limitZellen + 1; i++) {
-  document.write('<td class="zelle"><input name="z' + i +'" onclick="aend(' + i +')" value=" " maxlength="4" class="output bgtransparent coursorpointer" onfocus="if(this.blur)this.blur()" type="reset"></td>');
-}
-document.write("</tr>");
+
+writeBrailleline();
